616bf63f22e4bf8415b8fda23d73c2b562b160ae
BADAVATH SRIKANTH <80474350+SRIKANTH284@users.noreply.github.com>
2023-01-24 12:42:23 +0530

    Add files via upload

---
+++ impressive.js
@@ -0,0 +1,10 @@
+
+const left = document.getElementById("left-side");
+
+const handleMove = e => {
+  left.style.width = `${e.clientX / window.innerWidth * 100}%`;
+}
+
+document.onmousemove = e => handleMove(e);
+
+document.ontouchmove = e => handleMove(e.touches[0]);
